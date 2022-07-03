b650aaa62a5d5dafb17ef09ed72f4034f27346cb
safikh <safiuddin.khaja@gmail.com>
2022-07-03 20:40:35 +0530

    tests: Refactor tests

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+DATADIR = os.path.join(os.getcwd(), "tests")
--- tests/test_target_categorizer.py
+++ tests/test_target_categorizer.py
@@ -1,9 +1,10 @@
-import pytest
 import numpy as np
 import pandas as pd
 
 from featbox.target_categorizer import target_categorizer
 
+from tests import DATADIR
+
 def test_tc_1():
-    df = pd.read_csv('cardata.csv')
+    df = pd.read_csv(DATADIR + '/cardata.csv')
     assert target_categorizer(df['Make'], df['MSRP'], cuts=4).nunique() == 4
